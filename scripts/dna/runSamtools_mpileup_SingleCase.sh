265f8a568dd5199d34ffa29b190d71c79b776c39
BenF777 <ben.flies@lns.etat.lu>
2016-12-30 14:14:04 +0100

    deleted unnecessary files and lines

--- scripts/dna/runSamtools_mpileup_SingleCase.sh
+++ scripts/dna/runSamtools_mpileup_SingleCase.sh
@@ -10,39 +10,23 @@ echo "run samtools"
 OUT=$OUT_PATH/$(basename $FILENAME)
 OUT=${OUT%_aligned.bam}
 OUT=${OUT}_MPILEUP
+
+echo $FILENAME
 echo $OUT
 
-#echo $FILENAME #$OUT
 REFERENCE=/home/benflies/NGS/references/hg19/ucsc.hg19.fa
-echo $FILENAME
+
 samtools mpileup -d 1000000 -u -f $REFERENCE $FILENAME | bcftools call -cv -V indels > ${OUT}.bcf
 bcftools view ${OUT}.bcf > ${OUT}.vcf
 bgzip -c $OUT.vcf > $OUT.vcf.gz
 tabix -p vcf $OUT.vcf.gz
 
-#vcftools --vcf ${OUT}.vcf --recode --recode-INFO-all --bed $BED_FILE --out  ${OUT}_comp
-#mv ${OUT}_comp.recode.vcf  ${OUT}_comp.vcf
-
-#vcftools --vcf  ${OUT}_comp.vcf --remove-indels --recode --recode-INFO-all --out ${OUT}_comp_SNP
-#bgzip -c ${OUT}_comp_SNP.recode.vcf > ${OUT}_comp_SNP.recode.vcf.gz
-#tabix ${OUT}_comp_SNP.recode.vcf.gz
-
-#vcftools --vcf  ${OUT}_comp.vcf --keep-only-indels --recode --recode-INFO-all --out ${OUT}_comp_INDEL
-#bgzip -c ${OUT}_comp_INDEL.recode.vcf > ${OUT}_comp_INDEL.recode.vcf.gz
-#tabix ${OUT}_comp_INDEL.recode.vcf.gz
-
 #Filter
 echo "FILTER $OUT"
-
 Rscript $SCRIPT_PATH/dna/VariantFilter_mpileup_SNP.R 500 0.05 0 ${OUT}.vcf.gz
-#Rscript $SCRIPT_PATH/dna/VariantFilter_mpileup_INDEL.R 40 ${OUT}_comp_INDEL.recode.vcf.gz
-#
+
 #variant annotation
 echo "VARIANT ANNOTATION"
 bash $SCRIPT_PATH/dna/anovar_annotate.sh $CONFIG_FILE  $(ls -d -1 ${OUT}_filtered_DP500_AF0.05.vcf)
-#bash $SCRIPT_PATH/dna/anovar_annotate.sh $CONFIG_FILE  $(ls -d -1 ${OUT}_comp_INDEL.recode_filtered*.vcf)
-#
-#Rscript $SCRIPT_PATH/dna/annovar_csv2xlsx_mpileup.R  $(ls -d -1 ${OUT}_comp_SNP.recode_filtered*.hg19_multianno.csv)
-#Rscript $SCRIPT_PATH/dna/annovar_csv2xlsx_mpileup.R  $(ls -d -1 ${OUT}_comp_INDEL.recode_filtered*.hg19_multianno.csv)
-#
+
 echo "$OUT finished"
